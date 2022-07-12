9fb194caacca087638b81c6115836a02cd2d2da1
manishvbn <manishvbn@gmail.com>
2022-07-12 15:12:09 +0530

    External CSS

---
+++ Day1/react-core-app/src/components/3_component-with-css/ComponentOne.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const card = {
+    margin: '1em',
+    paddingLeft: 0,
+    border: '2px dashed green'
+};
+
+const ComponentOne = () => {
+    return (
+        <>
+            <h2 className='text-success'>Hello from Component One</h2>
+            <h2 style={card}>From Component One</h2>
+        </>
+    );
+};
+
+export default ComponentOne;
