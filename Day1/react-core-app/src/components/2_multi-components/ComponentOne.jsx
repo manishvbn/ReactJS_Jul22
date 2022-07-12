03aa7a37988a4530f7ba173b620b87746570085d
manishvbn <manishvbn@gmail.com>
2022-07-12 13:50:02 +0530

    Multi Components

---
+++ Day1/react-core-app/src/components/2_multi-components/ComponentOne.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComponentOne = () => {
+    return (
+        <h2 className='text-success'>
+            Hello from Component One
+        </h2>
+    );
+};
+
+export default ComponentOne;
