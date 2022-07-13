6f3354b3fc183a98ddd1e125e9cd9f05ad2681cd
manishvbn <manishvbn@gmail.com>
2022-07-13 10:21:54 +0530

    LifeCycleDemo

--- Day2/react-core-app/src/components/root/RootComponent.jsx
+++ Day2/react-core-app/src/components/root/RootComponent.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable */
 
 import React from 'react';
+import ClassVsFunctionalComponent from '../1_class-vs-function/ClassVsFunctionalComponent';
+import LCDemoComponent from '../2_lifecycle-demo/LCDemoComponent';
 
 const RootComponent = () => {
     return (
         <div className='container'>
+            {/* <ClassVsFunctionalComponent /> */}
+            <LCDemoComponent />
         </div>
     );
 };
