3fba1391fe2ae13b45e1c26b7f5a89d64a0dcfd6
manishvbn <manishvbn@gmail.com>
2022-07-15 08:57:20 +0530

    Day4 Start

---
+++ Day4/2_react-redux/react-redux-app/src/routes/index.js
@@ -0,0 +1,42 @@
+import React, { lazy, Suspense } from 'react';
+import { Link, Route, Switch, useLocation } from "react-router-dom";
+
+// Eager Loading
+import HomeComponent from "../components/home/HomeComponent";
+import LoaderAnimation from '../components/common/LoaderAnimation';
+
+// Lazy Loading
+const AboutComponent = lazy(() => import("../components/about/AboutComponent"));
+
+const img404 = require('../assets/http-404.jpg');
+
+export default (
+    <Suspense fallback={<LoaderAnimation />}>
+        <Switch>
+            <Route exact path="/" component={HomeComponent} />
+            <Route path="/about" component={AboutComponent} />
+            <Route path="*">
+                <NoMatch />
+            </Route>
+        </Switch>
+    </Suspense>
+);
+
+function NoMatch() {
+    let location = useLocation();
+
+    return (
+        <div className="text-center">
+            <article>
+                <h1 className="text-danger">No match for <code>{location.pathname}</code></h1>
+                <h4 className="text-danger">Please check your Route Configuration</h4>
+                <div className="mt-5">
+                    <img src={img404} alt="Not Found" className="rounded" />
+                </div>
+                <h2 className="mt-5">
+                    <Link className="nav-link" to="/">Back to Home</Link>
+                </h2>
+            </article>
+        </div>
+    );
+}
