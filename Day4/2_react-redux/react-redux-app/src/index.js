4c07853e472c9b7b4c21aba954d2d3b51b16af5a
manishvbn <manishvbn@gmail.com>
2022-07-15 12:27:12 +0530

    ReduxStart

--- Day4/2_react-redux/react-redux-app/src/index.js
+++ Day4/2_react-redux/react-redux-app/src/index.js
@@ -7,9 +7,41 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap';
 import RootComponent from './components/root/RootComponent';
 
+import configureStore from './store/configureStore';
+import * as counterActions from './actions/counterActions';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <RootComponent />
   // </React.StrictMode>
 );
+
+// const appStore = configureStore();
+// console.log(appStore);
+// console.log(appStore.getState());
+
+// const appStore = configureStore({ counterReducer: 1000 });
+// console.log(appStore);
+// console.log(appStore.getState());
+
+const appStore = configureStore();
+console.log("Loaded State:", appStore.getState());
+
+appStore.subscribe(() => {
+  console.warn("Store State Change Detected");
+  console.log("New State:", appStore.getState());
+});
+
+// Take the actions to be performed from Action Creator
+// Dispatch actions to modify the store
+
+// Actions
+let incAction = counterActions.incCounter();
+let decAction = counterActions.decCounter();
+
+// Dispatching Actions
+// When we dispatch the action, reducer will take it and modify the store
+appStore.dispatch(incAction);
+appStore.dispatch(incAction);
+appStore.dispatch(decAction);
